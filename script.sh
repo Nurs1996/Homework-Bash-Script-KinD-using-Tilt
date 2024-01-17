7eb18325ecf49034cac74eaed9340ef20fdfbf9b
Nurs Nurbekov <126831811+Nurs1996@users.noreply.github.com>
2024-01-17 13:42:39 -0600

    Update script.sh
    updated script wih custom port

--- script.sh
+++ script.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 kind create cluster --config kind-config.yaml
-docker run -d -p 5000:5000 --restart=always --name registry registry:2
-docker build -t localhost:5000/custom-nginx:latest -f Dockerfile .
-docker push localhost:5000/custom-nginx:latest
+
+docker network create kind-registry
+docker run -d --name kind-registry --network=kind-registry -p 8081:8081 registry:2
+
+kubectl apply -f kind-registry.yaml
+
+docker build -t localhost:8081/custom-nginx:latest -f Dockerfile .
+docker push localhost:8081/custom-nginx:latest
 
 tilt up
